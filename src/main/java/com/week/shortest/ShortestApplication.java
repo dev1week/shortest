7fe2356161c066a2f4a8835613f478340f60ce33
dev1week <rondo2860@gmail.com>
2023-09-25 20:09:49 +0900

    Initial commit

---
+++ src/main/java/com/week/shortest/ShortestApplication.java
@@ -0,0 +1,13 @@
+package com.week.shortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/week/shortest/ShortestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.week.shortest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
