4da2ce48032f72dd42d59b9f0c330ca5fb979ea7
dev1week <rondo2860@gmail.com>
2023-10-26 07:59:25 +0900

    feat: 약국 도메인 리포지토리 개발(#9)

---
+++ src/main/java/com/week/shortest/pharmacy/repository/PharmacyRepository.java
@@ -0,0 +1,9 @@
+package com.week.shortest.pharmacy.repository;
+
+import com.week.shortest.pharmacy.entity.Pharmacy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
+
+
+}
