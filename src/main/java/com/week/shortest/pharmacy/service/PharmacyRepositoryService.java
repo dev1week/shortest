131c2f657351dc6d6cfffeb6b10762fd7069b84e
dev1week <rondo2860@gmail.com>
2023-10-27 20:17:10 +0900

    chore: 약국데이터 업로드 (#9)

--- src/main/java/com/week/shortest/pharmacy/service/PharmacyRepositoryService.java
+++ src/main/java/com/week/shortest/pharmacy/service/PharmacyRepositoryService.java
@@ -5,8 +5,10 @@ import com.week.shortest.pharmacy.repository.PharmacyRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+
+import java.util.List;
 import java.util.Objects;
 
 @Slf4j
@@ -27,6 +29,11 @@ public class PharmacyRepositoryService {
         }
 
         entity.changePharmacyAddress(address);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Pharmacy> findAll(){
+        return pharmacyRepository.findAll();
     }
 
 
