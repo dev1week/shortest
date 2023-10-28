41bdf36a9b869a0752ae842d9c94de64bb74ffd1
dev1week <rondo2860@gmail.com>
2023-10-28 17:48:00 +0900

    feat: 약국 전체 조회 메서드 작성(#11)

---
+++ src/main/java/com/week/shortest/pharmacy/service/PharmacySearchService.java
@@ -0,0 +1,51 @@
+package com.week.shortest.pharmacy.service;
+
+import com.week.shortest.pharmacy.dto.PharmacyDto;
+import com.week.shortest.pharmacy.entity.Pharmacy;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PharmacySearchService {
+
+
+    private final PharmacyRepositoryService pharmacyRepositoryService;
+
+
+    public List<PharmacyDto> searchPharmacyDtoList(){
+
+
+
+        //db 관련 로직
+
+        List<Pharmacy> searchResult = pharmacyRepositoryService.findAll();
+        List<PharmacyDto> convertedResult = new ArrayList<>();
+        for(Pharmacy pharmacy : searchResult){
+                convertedResult.add(convertToDto(pharmacy));
+        }
+
+        return convertedResult;
+    }
+
+    private PharmacyDto convertToDto(Pharmacy pharmacy){
+        return PharmacyDto.builder()
+                            .id(pharmacy.getId())
+                            .pharmacyAddress(pharmacy.getPharmacyAddress())
+                            .pharmacyName(pharmacy.getPharmacyName())
+                            .latitude(pharmacy.getLatitude())
+                            .longitude(pharmacy.getLongitude())
+                            .build();
+
+
+
+
+
+    }
+
+}
